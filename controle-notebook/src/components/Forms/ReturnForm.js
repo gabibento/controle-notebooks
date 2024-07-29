4b8860722c5e0167935b8692fb1469758683b30d
Gabriella <gabriella.mabento@gmail.com>
2024-07-29 18:07:53 -0300

    adicionada função getLastHistory

--- controle-notebook/src/components/Forms/ReturnForm.js
+++ controle-notebook/src/components/Forms/ReturnForm.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import useHistoryForm from './useHistoryForm'
-import { useContext } from 'react';
+import { db } from '../../firebaseConfig';
+import { collection, doc, getDocs, limit, orderBy, query } from "firebase/firestore";
 
 const ReturnForm = () => {
 
     const { history, handleOnChange, handleOnSubmit } = useHistoryForm();
 
-    console.log(history)
+    const getLastHistory = async (notebookId) => {
+      try {
+        const historyRef = collection(db, 'notebooks', notebookId, 'history');
+        // Ordenar por timestamp e limitar a um resultado
+        const q = query(historyRef, orderBy('createdAt', 'desc'), limit(1));
+        const querySnapshot = await getDocs(q);
+    
+        let lastHistory = null;
+        querySnapshot.forEach((doc) => {
+          lastHistory = { id: doc.id, ...doc.data() };
+        });
+    
+        return lastHistory;
+      } catch (e) {
+        console.error('Erro ao obter o último histórico: ', e);
+        return null;
+      }
+    }
 
   return (
     <form onSubmit={handleOnSubmit}>
--- controle-notebook/src/components/Forms/getHistory.js
+++ controle-notebook/src/components/Forms/getHistory.js
@@ -1,9 +1,21 @@
 import { db } from '../../firebaseConfig';
-import { collection, doc, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import { collection, doc, getDocs, limit, orderBy, query } from "firebase/firestore";
 
 export const getHistory = async (notebookId) => {
-    const historyRef = collection(db, "notebooks", notebookId, "history")
-    const q = query(historyRef, orderBy("createdAt", "desc"), limit(1))
-    const querySnapshot = await getDocs(q)
+    try {
+        const historyRef = collection(db, 'notebooks', notebookId, 'history');
+      
+        const q = query(historyRef, orderBy('createdAt', 'desc'), limit(1));
+        const querySnapshot = await getDocs(q);
     
+        let lastHistory = null;
+        querySnapshot.forEach((doc) => {
+          lastHistory = { id: doc.id, ...doc.data() };
+        });
+    
+        return lastHistory;
+      } catch (e) {
+        console.error('Erro ao obter o último histórico: ', e);
+        return null;
+      }
 }
