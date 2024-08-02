f9782b9efa49ab650c270ee12605af8efbaefac0
Gabriella Maurea Bento <gabriella.mabento@gmail.com>
2024-08-01 21:51:11 -0300

    feat: validação de senha

--- controle-notebook/src/components/Forms/Form.js
+++ controle-notebook/src/components/Forms/Form.js
@@ -5,6 +5,7 @@ import { collection, getDocs } from 'firebase/firestore';
 
 const Form = ({history, handleOnChange, handleOnSubmit}) => {
   const [collaborators, setCollaborators] = useState([]);
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
     const fetchCollaborators = async () => {
@@ -20,8 +21,29 @@ const Form = ({history, handleOnChange, handleOnSubmit}) => {
     fetchCollaborators();
   }, []);
 
+  const validatePassword = (password, collaboratorName) => {
+    const collaborator = collaborators.find(c => c.name === collaboratorName);
+    return collaborator && collaborator.password === password;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(password)
+    if (validatePassword(password, history.collaborator)) {
+      handleOnSubmit(e);
+    } else {
+      console.log('Invalid password');
+      
+    }
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <form onSubmit={handleOnSubmit}>
+    <form onSubmit={handleSubmit}>
       
     <label htmlFor="collaborator">Nome</label>
     <select name="collaborator" id="collaborator" onChange={handleOnChange} required>
@@ -36,6 +58,8 @@ const Form = ({history, handleOnChange, handleOnSubmit}) => {
     id='password'
     name='password'
     type="password"
+    value={password}
+    onChange={handlePasswordChange}
     required
     />
     
